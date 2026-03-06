66b30f7b5cb02c0de9dcdeab6d19c1c16aafa2db
Mocey2 <gracemahouk@gmail.com>
2026-03-06 18:33:37 +0100

    Auto load data et superuser dans build

--- portfolio-backend/build.sh
+++ portfolio-backend/build.sh
@@ -6,3 +6,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input --clear
 
 python manage.py migrate
+
+python manage.py load_initial_data
+
+python manage.py createsuperuser --noinput --email "${DJANGO_SUPERUSER_EMAIL:-admin@example.com}" --username "${DJANGO_SUPERUSER_USERNAME:-admin}" 2>/dev/null || true
